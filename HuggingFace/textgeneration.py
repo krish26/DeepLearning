fd0304176d97dd178e95f837707afb49fbc0b9ad
krishna koumudi <koumudi26@gmail.com>
2026-03-17 18:34:27 +0100

    flan t5

--- HuggingFace/textgeneration.py
+++ HuggingFace/textgeneration.py
@@ -2,17 +2,20 @@ from transformers import pipeline
 
 gen = pipeline("text2text-generation", model ="google/flan-t5-base")
 
-prompt = (
-    "Produce exactly ONE family-friendly joke "
-    "one sentence , 10-20 words, end with a period")
+prompt = """
+Q: What is a smartphone?
+A: A smartphone is a portable device that combines communication, computing, and internet access.
 
-result = gen(
+Q: What is a computer?
+A:
+"""
+result = gen( 
     prompt,
-    max_new_tokens=32,
-    num_beams=5,
-    no_repeat_ngram_size=3,
-    do_sample=True
-)
+    max_new_tokens=32, 
+    num_beams=5, 
+    no_repeat_ngram_size=3, 
+    do_sample=False
+ )
 
 print("\n-- TEXT GENERATION - FLAN T5")
 print(result[0]["generated_text"])
